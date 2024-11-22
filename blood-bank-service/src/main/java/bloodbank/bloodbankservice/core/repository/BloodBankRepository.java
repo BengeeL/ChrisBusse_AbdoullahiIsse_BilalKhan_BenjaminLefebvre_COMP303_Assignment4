c13162ee07f032ef9b1ccf5c8bcfcd2b6885d054
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-11-22 11:58:41 -0500

    ref: blood bank service and repository (complete)

--- blood-bank-service/src/main/java/bloodbank/bloodbankservice/core/repository/BloodBankRepository.java
+++ blood-bank-service/src/main/java/bloodbank/bloodbankservice/core/repository/BloodBankRepository.java
@@ -1,10 +1,13 @@
 package bloodbank.bloodbankservice.core.repository;
 
 import bloodbank.bloodbankservice.core.entities.BloodBank;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface BloodBankRepository extends CrudRepository<BloodBank, Long> {
-    // TODO: Add anything else within here.
+public interface BloodBankRepository extends JpaRepository<BloodBank, Long> {
+    List<BloodBank> findBloodBanksByBloodBankName(final String name);
+    BloodBank findBloodBankByBloodBankName(final String name);
 }
--- blood-bank-service/src/main/java/bloodbank/bloodbankservice/core/service/BloodBankService.java
+++ blood-bank-service/src/main/java/bloodbank/bloodbankservice/core/service/BloodBankService.java
@@ -1,11 +1,30 @@
 package bloodbank.bloodbankservice.core.service;
 
+import bloodbank.bloodbankservice.core.entities.BloodBank;
 import bloodbank.bloodbankservice.core.repository.BloodBankRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * Service class for managing BloodBank entities.
+ * @author Muhammad Bilal Khan
+ * @version 1.0.0
+ * @since 1.0.0
+ * @see bloodbank.bloodbankservice.core.entities.BloodBank
+ * @see bloodbank.bloodbankservice.core.repository.BloodBankRepository
+ * @see bloodbank.bloodbankservice.core.dtos.BloodBankDto
+ */
 @Service
 public class BloodBankService {
+    //region DEFAULTS
+    private static final String BLOOD_BANK_NOT_FOUND = "BloodBank with id %s not found";
+    //endregion
+
     private final BloodBankRepository bloodBankRepository;
 
     @Autowired
@@ -13,5 +32,68 @@ public class BloodBankService {
         this.bloodBankRepository = bloodBankRepository;
     }
 
-    // TODO: Add service logic here.
+    public List<BloodBank> getAllBloodBanks() {
+        return bloodBankRepository.findAll();
+    }
+
+    public List<BloodBank> getBloodBanksByName(final String name) {
+        return bloodBankRepository
+                .findBloodBanksByBloodBankName(name);
+    }
+
+    public BloodBank getBloodBankById(final Long id) throws EntityNotFoundException {
+        // @note: throws EntityNotFoundException
+        return findBloodBankOrThrowException(id);
+    }
+
+    public BloodBank getBloodBankByName(final String name) throws EntityNotFoundException {
+        return bloodBankRepository
+                .findBloodBankByBloodBankName(name);
+    }
+
+    public BloodBank saveBloodBank(final @Valid BloodBank bloodBank) {
+        return bloodBankRepository
+                .save(bloodBank);
+    }
+
+    public void saveBloodBanks(final List<@Valid BloodBank> bloodBanks) {
+        bloodBankRepository.saveAll(bloodBanks);
+    }
+
+    public void updateBloodBank(final @Valid BloodBank bloodBank, final Long id) throws EntityNotFoundException  {
+        // @note: throws EntityNotFoundException
+        var bloodBankToUpdate = findBloodBankOrThrowException(id);
+
+        updateBloodBankAttributes(bloodBankToUpdate, bloodBank);
+        bloodBankRepository.save(bloodBankToUpdate);
+    }
+
+    public Boolean deleteBloodBank(final Long id) throws EntityNotFoundException {
+        // @note: throws EntityNotFoundException
+        var bloodBankToDelete = findBloodBankOrThrowException(id);
+
+        bloodBankRepository.delete(bloodBankToDelete);
+        return true;
+    }
+
+    //region Class Helpers
+    private BloodBank findBloodBankOrThrowException(final Long id) throws EntityNotFoundException {
+        return bloodBankRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.format(BLOOD_BANK_NOT_FOUND, id)));
+    }
+
+    private void updateBloodBankAttributes(
+            final BloodBank target,
+            final BloodBank source
+    ) {
+        target.setBloodBankName(source.getBloodBankName());
+        target.setAddress(source.getAddress());
+        target.setCity(source.getCity());
+        target.setPhoneNumber(source.getPhoneNumber());
+        target.setWebsite(source.getWebsite());
+        target.setEmail(source.getEmail());
+        target.setModifiedAt(new Timestamp(System.currentTimeMillis()));
+    }
+    //endregion
 }
